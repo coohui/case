73026814de53565f27dfd7ceb119a73b9162c902
coohui <luizhuqi1543@163.com>
2018-05-23 23:37:45 +0800

    kugou

---
+++ 微信小程序/utils/util.js
@@ -0,0 +1,60 @@
+// const formatTime = date => {
+//   const year = date.getFullYear()
+//   const month = date.getMonth() + 1
+//   const day = date.getDate()
+//   const hour = date.getHours()
+//   const minute = date.getMinutes()
+//   const second = date.getSeconds()
+
+//   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+// }
+
+// const formatNumber = n => {
+//   n = n.toString()
+//   return n[1] ? n : '0' + n
+// }
+
+
+// // 显示繁忙提示
+// var showBusy = text => wx.showToast({
+//     title: text,
+//     icon: 'loading',
+//     duration: 10000
+// })
+
+// // 显示成功提示
+// var showSuccess = text => wx.showToast({
+//     title: text,
+//     icon: 'success'
+// })
+
+// // 显示失败提示
+// var showModel = (title, content) => {
+//     wx.hideToast();
+
+//     wx.showModal({
+//         title,
+//         content: JSON.stringify(content),
+//         showCancel: false
+//     })
+// }
+
+var utils={
+  msgAlert: function (msg) {
+    wx.showModal({
+      title: '提示',
+      content: msg,
+      success: function (res) {
+        console.log(res)
+        if (res.confirm) {
+          console.log('用户点击了确定')
+        } else {
+          console.log('用户点击了取消')
+        }
+      }
+    })
+  }
+}
+
+module.exports = utils;
+//{ formatTime, showBusy, showSuccess, showModel }
